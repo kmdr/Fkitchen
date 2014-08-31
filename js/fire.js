f73a3ad928605fd678a30db71d2677a8e7c06761
kmdr <20.10.ak.tkb@gmail.com>
2014-08-31 14:19:20 +0900

    肉が切れた！　火がついた！

---
+++ js/fire.js
@@ -0,0 +1,40 @@
+
+
+
+$(function(){
+
+	var fireFlag = 0;
+	console.log(fireFlag);
+
+	$(".gas .gasSwitch").rotate({
+        bind:
+            {
+            click: function(){
+                    
+            		if(fireFlag == 0){
+	                    $(this).rotate({
+	                        angle: 0,
+	                        animateTo: 120,
+	                        easing: $.easing.easeInOutExpo
+	                    })
+	                    fireFlag = 1;
+
+	                    $(".gas").css("background", "url(\"images/utensil/img_gas_on.png\")");
+
+	                }else{
+	                    $(this).rotate({
+	                        angle: 120,
+	                        animateTo: 0,
+	                        easing: $.easing.easeInOutExpo
+	                    })
+	                    fireFlag = 0;
+	                    $(".gas").css("background", "url(\"images/utensil/img_gas_off.png\")");
+
+	                }
+
+                }
+            }
+    });
+
+	console.log(fireFlag);
+});
